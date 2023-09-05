6b6ce4a4820f4fd01970ccd957491e9562394e68
95decode <95decode@gmail.com>
2023-09-05 22:00:14 +0900

    Apply domains

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <div className="fixed w-full z-50">
       <div className="h-[54px] pc:h-[100px] flex items-center justify-center bg-ym-blue">
-        <div className="absolute invisible pc:visible">
+        <div className="hidden pc:block">
           <Image
             className="hover:cursor-pointer"
             src={`/images/BannerPC.png`}
@@ -19,7 +19,7 @@ export const Header = () => {
             }}
           />
         </div>
-        <div className="absolute visible pc:invisible">
+        <div className="block pc:hidden">
           <Image
             className="hover:cursor-pointer"
             src={`/images/Banner.png`}
@@ -33,8 +33,13 @@ export const Header = () => {
         </div>
       </div>
       <div className="h-[50px] pc:h-[89px] flex items-center justify-center bg-white border-b border-b-ym-border">
-        <div className="flex justify-between w-[90vw] pc:w-[50vw] gap-[10px] pc:gap-[3.125vw] pc:text-[20px] text-[14px]">
-          <div className="group h-[50px] pc:h-[89px] pc:w-full flex flex-col items-center hover:cursor-pointer">
+        <div className="flex justify-between w-[90vw] pc:mx-[25vw] gap-[10px] pc:gap-[3.125vw] pc:text-[20px] text-[14px]">
+          <div
+            className="group h-[50px] pc:h-[89px] pc:w-full flex flex-col items-center hover:cursor-pointer"
+            onClick={() => {
+              router.push('/');
+            }}
+          >
             <div className="h-[50px] pc:h-[89px] flex items-center">
               병원 소개
             </div>
@@ -46,37 +51,52 @@ export const Header = () => {
               } w-full pc:w-[115px]`}
             ></div>
           </div>
-          <div className="group h-[50px] pc:h-[89px] pc:w-full flex flex-col items-center hover:cursor-pointer">
+          <div
+            className="group h-[50px] pc:h-[89px] pc:w-full flex flex-col items-center hover:cursor-pointer"
+            onClick={() => {
+              router.push('/prostate');
+            }}
+          >
             <div className="h-[50px] pc:h-[89px] flex items-center">
               전립선 치료
             </div>
             <div
               className={`border ${
-                router.asPath === '/c'
+                router.asPath === '/prostate'
                   ? 'border-black'
                   : 'group-hover:border-black border-transparent'
               } w-full pc:w-[115px]`}
             ></div>
           </div>
-          <div className="group h-[50px] pc:h-[89px] pc:w-full flex flex-col items-center hover:cursor-pointer">
+          <div
+            className="group h-[50px] pc:h-[89px] pc:w-full flex flex-col items-center hover:cursor-pointer"
+            onClick={() => {
+              // router.push('/thyroid');
+            }}
+          >
             <div className="h-[50px] pc:h-[89px] flex items-center">
               갑상선 치료
             </div>
             <div
               className={`border ${
-                router.asPath === '/c'
+                router.asPath === '/thyroid'
                   ? 'border-black'
                   : 'group-hover:border-black border-transparent'
               } w-full pc:w-[115px]`}
             ></div>
           </div>
-          <div className="group h-[50px] pc:h-[89px] pc:w-full flex flex-col items-center hover:cursor-pointer">
+          <div
+            className="group h-[50px] pc:h-[89px] pc:w-full flex flex-col items-center hover:cursor-pointer"
+            onClick={() => {
+              router.push('/directions');
+            }}
+          >
             <div className="h-[50px] pc:h-[89px] flex items-center">
               오시는 길
             </div>
             <div
               className={`border ${
-                router.asPath === '/c'
+                router.asPath === '/directions'
                   ? 'border-black'
                   : 'group-hover:border-black border-transparent'
               } w-full pc:w-[115px]`}
